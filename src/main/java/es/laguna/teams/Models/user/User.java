91c5e6361284ff500a803565bae5a94f19058b3f
Haroun Zemrani <harounzemrani4@gmail.com>
2024-02-11 18:53:24 +0100

    Comienzo del security

---
+++ src/main/java/es/laguna/teams/Models/user/User.java
@@ -0,0 +1,66 @@
+package es.laguna.teams.Models.user;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "user_")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public User(String email, String password){
+        this.email=email;
+        this.password=password;
+    }
+    @Enumerated(EnumType.STRING)
+    private Role role=Role.USER;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    //Se utiliza el return password y email por que son los que vamos a utilizar para iniciar sesion
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+    //Se tienen que poner todos en true el alain lo dijo
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true ;
+    }
+}
