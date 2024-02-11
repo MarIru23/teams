59017bdf110f065f456b62aa4ab38d037d7332ba
Mario Iruela <marioiruelaro@gmail.com>
2024-02-11 18:01:33 +0100

    DATA SQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Getafe', 'España', '#ff00ff');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Liverpool', 'Inglaterra', '#ff00gh');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Juventus', 'Italia', '#ff00gf');
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Borja Mayoral', '10', 1);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Greenwood', '12', 1);
