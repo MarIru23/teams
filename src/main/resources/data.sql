5c265c2e711bf53a89d52c02a8284cfde87f75a6
Mario Iruela <marioiruelaro@gmail.com>
2024-02-12 13:45:17 +0100

    DATA SQL UPDATED

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,68 @@
 INSERT INTO Team (uuid, name, country, color)
-VALUES(uuid(), 'Getafe', 'España', '#ff00ff');
+VALUES(uuid(), 'Barcelona', 'España', '#ff0000');
 
 INSERT INTO Team (uuid, name, country, color)
-VALUES(uuid(), 'Liverpool', 'Inglaterra', '#ff00gh');
+VALUES(uuid(), 'Real Madrid', 'España', '#ffffff');
 
 INSERT INTO Team (uuid, name, country, color)
-VALUES(uuid(), 'Juventus', 'Italia', '#ff00gf');
+VALUES(uuid(), 'Manchester City', 'Inglaterra', '#6FA8DC');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Bayern Munich', 'Alemania', '#DC143C');
+
 
 INSERT INTO Player (uuid, name, number, Team_id)
-VALUES(uuid(), 'Borja Mayoral', '10', 1);
+VALUES(uuid(), 'Lionel Messi', '10', 1); -- Barcelona
 
 INSERT INTO Player (uuid, name, number, Team_id)
-VALUES(uuid(), 'Greenwood', '12', 1);
+VALUES(uuid(), 'Gerard Piqué', '3', 1); -- Barcelona
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Ansu Fati', '22', 1); -- Barcelona
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Sergio Busquets', '5', 1); -- Barcelona
+
+-- Players for Real Madrid
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Karim Benzema', '9', 2); -- Real Madrid
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Toni Kroos', '8', 2); -- Real Madrid
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Luka Modric', '10', 2); -- Real Madrid
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Sergio Ramos', '4', 2); -- Real Madrid
+
+-- Players for Manchester City
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Kevin De Bruyne', '17', 3); -- Manchester City
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Raheem Sterling', '7', 3); -- Manchester City
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Phil Foden', '47', 3); -- Manchester City
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Ruben Dias', '3', 3); -- Manchester City
+
+-- Players for Bayern Munich
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Robert Lewandowski', '9', 4); -- Bayern Munich
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Thomas Müller', '25', 4); -- Bayern Munich
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Joshua Kimmich', '6', 4); -- Bayern Munich
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Manuel Neuer', '1', 4); -- Bayern Munich
+
+
 
 
 INSERT INTO user_ (email, password)
