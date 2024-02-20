f485d8284b5718c3576aaf9276a452db49ee1cb0
Haroun Zemrani <harounzemrani4@gmail.com>
2024-02-21 00:03:39 +0100

    cosas prueba

---
+++ src/main/resources/data.sql
@@ -0,0 +1,108 @@
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Getafe', 'España', '#ff00ff');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Liverpool', 'Inglaterra', '#ff00gh');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Juventus', 'Italia', '#ff00gf');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Barcelona', 'España', '#ff0000');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Real Madrid', 'España', '#ffffff');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Manchester City', 'Inglaterra', '#6FA8DC');
+
+INSERT INTO Team (uuid, name, country, color)
+VALUES(uuid(), 'Bayern Munich', 'Alemania', '#DC143C');
+
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Borja Mayoral', '10', 1);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Greenwood', '12', 1);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Luis Milla', '5', 1);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Mauro Arambarri', '8', 1);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Alisson Becker', '1', 2);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Joe Gomez', '2', 2);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Harvey Elliott', '19', 2);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Luis Díaz', '7', 2);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Bremer', '3', 3);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Alex Sandro', '12', 3);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Daniele Rugani', '24', 3);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Federico Chiesa', '7', 3);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Lionel Messi', '10', 4);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Gerard Piqué', '3', 4);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Ansu Fati', '22', 4);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Sergio Busquets', '5', 4);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Karim Benzema', '9', 5);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Toni Kroos', '8', 5);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Luka Modric', '10', 5);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Sergio Ramos', '4', 5);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Kevin De Bruyne', '17', 6);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Raheem Sterling', '7', 6);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Phil Foden', '47', 6);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Ruben Dias', '3', 6);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Robert Lewandowski', '9', 7);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Thomas Müller', '25', 7);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Joshua Kimmich', '6', 7);
+
+INSERT INTO Player (uuid, name, number, Team_id)
+VALUES(uuid(), 'Manuel Neuer', '1', 7);
+
+INSERT INTO user_(email, password)
+VALUES('user','$2a$12$K4tojeaYWMK55KzWzDWtLOuuUjRTkycWhSGHYWA2LXMZqmZUtuXPO')
