9bb821fb87d332893694e828476bb0104635b19a
Haroun Zemrani <harounzemrani4@gmail.com>
2024-02-21 00:21:17 +0100

    cosas prueba

--- src/main/java/es/laguna/teams/Repositories/factories/PlayerTeamFactory.java
+++ src/main/java/es/laguna/teams/Repositories/factories/PlayerTeamFactory.java
@@ -4,18 +4,16 @@ import es.laguna.teams.Models.Player;
 import es.laguna.teams.Models.Team;
 import es.laguna.teams.Repositories.PlayerRepository;
 import es.laguna.teams.Repositories.TeamRepository;
-import jakarta.annotation.PostConstruct;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/factory")
 public class PlayerTeamFactory {
@@ -29,37 +27,32 @@ public class PlayerTeamFactory {
     }
 
     @PostMapping("/people")
-    public void init(){
-        try {
-            Faker faker = new Faker(new Locale("es"));
-            for (int i = 0; i < 5; i++) {
-                List<Player> playerList = new ArrayList<>();
-                Team team = new Team(
+    public void init() {
+        Faker faker = new Faker();
+        for (int i = 0; i < 5; i++) {
+            List<Player> playerList = new ArrayList<>();
+            Team team = new Team(
+                    null,
+                    UUID.randomUUID(),
+                    faker.team().name(),
+                    faker.address().country(),
+                    faker.color().hex(),
+                    null
+            );
+            teamRepository.save(team);
+            for (int j = 0; j < 10; j++) {
+                Player player = new Player(
                         null,
                         UUID.randomUUID(),
-                        faker.commerce().department(),
-                        faker.lorem().characters(10, 40),
-                        faker.color().hex(),
-                        null
+                        faker.name().fullName(),
+                        faker.number().numberBetween(1, 25),
+                        team
                 );
-                teamRepository.save(team);
-                for (int j = 0; j < 10; j++) {
-                    Player player = new Player(
-                            null,
-                            UUID.randomUUID(),
-                            faker.commerce().productName(),
-                            faker.number().numberBetween(1, 25),
-                            team
-                    );
-                    playerList.add(player);
-                    playerRepository.save(player);
-                }
-                team.setPlayers(playerList);
-                teamRepository.save(team);
+                playerList.add(player);
+                playerRepository.save(player);
             }
-        } catch (Exception e){
-
+            team.setPlayers(playerList);
+            teamRepository.save(team);
         }
     }
 }
-
--- src/main/java/es/laguna/teams/Repositories/factories/UserFactory.java
+++ src/main/java/es/laguna/teams/Repositories/factories/UserFactory.java
@@ -1,47 +1,37 @@
 package es.laguna.teams.Repositories.factories;
+
 import es.laguna.teams.Models.user.User;
 import es.laguna.teams.Repositories.UserDetailsRepository;
-import jakarta.annotation.PostConstruct;
-
-import jakarta.persistence.Column;
-import lombok.extern.slf4j.Slf4j;
 import net.datafaker.Faker;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
-@Component
-@Slf4j
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/factory")
 public class UserFactory {
-
     private final UserDetailsRepository userDetailsRepository;
 
     @Autowired
-    public UserFactory( UserDetailsRepository userDetailsRepository) {
+    public UserFactory(UserDetailsRepository userDetailsRepository) {
         this.userDetailsRepository = userDetailsRepository;
     }
 
-    @PostMapping ("/user")
-    public void init(){
-        try {
-            Faker faker = new Faker(new Locale("es"));
-            for (int i = 0; i < 3; i++) {
-                userDetailsRepository.save(
-                        new User(
-                                faker.internet().emailAddress(),
-                                faker.internet().password()
-                        )
-                );
-            }
-        } catch (Exception e){
-            log.info(e.getMessage());
+    @PostMapping("/users")
+    public void init() {
+        Faker faker = new Faker();
+        List<User> userList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            User user = new User(
+                    faker.internet().emailAddress(),
+                    faker.internet().password()
+            );
+            userList.add(user);
         }
+        userDetailsRepository.saveAll(userList);
     }
 }
